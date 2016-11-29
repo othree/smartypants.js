64a74863e1ce25b7aad956b18267f81891637bc6
othree <othree@gmail.com>
2016-11-29 15:25:29 +0800

    Regen file

--- smartypants.js
+++ smartypants.js
@@ -1,24 +1,33 @@
+/* eslint-env browser, amd, node */
+
 /**
- * Based on UMD template
- * https://github.com/umdjs/umd/blob/master/templates/commonjsStrictGlobal.js
+ *
+ * @file Translates plain ASCII punctuation characters into "smart" typographic punctuation
+ * @desc See the readme for details, installation instructions, and * license information.
+ * @version 0.0.4
+ * @author othree
+ *
+ * Copyright (c) 2003-2004 John Gruber
+ * Copyright (c) 2016      Kao, Wei-Ko(othree)
+ *
  */
 
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD
-        define('SmartyPants', ['exports'], function (exports) {
-            factory((root.SmartyPants = exports));
-        });
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-        // CommonJS
-        factory(exports);
-    } else {
-        // Browser globals
-        factory((root.SmartyPants = {}));
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define('SmartyPants', ['exports'], function (exports) {
+      factory((root.SmartyPants = exports));
+    });
+  } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    // CommonJS
+    factory(exports);
+  } else {
+    // Browser globals
+    factory((root.SmartyPants = {}));
+  }
 }(this, function (exports) {
 
-"use strict";
+  "use strict";
 var tags_to_skip = /<(\/?)(?:pre|code|kbd|script|math)[^>]*>/i;
 /**
  * @param text text to be parsed
