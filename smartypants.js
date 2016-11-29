270a8f6d478f4c6ceb09491b54f440c63c139a85
othree <othree@gmail.com>
2016-11-29 14:37:19 +0800

    UMD

--- smartypants.js
+++ smartypants.js
@@ -1,3 +1,23 @@
+/**
+ * Based on UMD template
+ * https://github.com/umdjs/umd/blob/master/templates/commonjsStrictGlobal.js
+ */
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define('SmartyPants', ['exports'], function (exports) {
+            factory((root.SmartyPants = exports));
+        });
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        // CommonJS
+        factory(exports);
+    } else {
+        // Browser globals
+        factory((root.SmartyPants = {}));
+    }
+}(this, function (exports) {
+
 "use strict";
 var tags_to_skip = /<(\/?)(?:pre|code|kbd|script|math)[^>]*>/i;
 /**
@@ -656,3 +676,6 @@ var smartypantsu = function (text, attr) {
 exports.smartypantsu = smartypantsu;
 exports.__esModule = true;
 exports.default = SmartyPants;
+
+
+}));
