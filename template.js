c868f67f646d66a3fca5e044916486cba8e2d6b4
othree <othree@gmail.com>
2016-11-30 11:38:01 +0800

    Update template

--- template.js
+++ template.js
@@ -16,20 +16,20 @@
 /*! smartypants.js 0.0.4 | (c) Kao, Wei-Ko(othree) | github.com/othree/smartypants.js/blob/master/LICENSE */
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD
-    define('SmartyPants', ['exports'], function (exports) {
-      factory((root.SmartyPants = exports));
-    });
-  } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-    // CommonJS
-    factory(exports);
-  } else {
-    // Browser globals
-    factory((root.SmartyPants = {}));
-  }
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define('SmartyPants', ['exports'], function (exports) {
+            factory((root.SmartyPants = exports));
+        });
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        // CommonJS
+        factory(exports);
+    } else {
+        // Browser globals
+        factory((root.SmartyPants = {}));
+    }
 }(this, function (exports) {
 
-  // @CODE
+// @CODE
 
 }));
