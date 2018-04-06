9a2d960bc5d6fc5c44daac8ae656e4a76da601ac
othree <othree@gmail.com>
2018-04-06 12:14:26 +0800

    Add attr -1 test

--- test/index.mjs
+++ test/index.mjs
@@ -1,10 +1,18 @@
 import test from 'tape';
 import { smartypantsu } from '../smartypants.mjs';
 
-test('timing test', function (t) {
+test('u', function (t) {
   t.plan(3);
 
   t.equal(smartypantsu('6\'2" tall', '1'), '6\u20192\u201d tall');
   t.equal(smartypantsu("``backticks''", '1'), '\u201cbackticks\u201d');
   t.equal(smartypantsu("A--B", '1'), 'A\u2014B');
+});
+
+test('un u', function (t) {
+  t.plan(1);
+
+  t.equal(smartypantsu('6\u20192\u201d tall', '-1'), '6\'2" tall');
+  // t.equal(smartypantsu("``backticks''", '1'), '\u201cbackticks\u201d');
+  // t.equal(smartypantsu("A--B", '1'), 'A\u2014B');
 });
